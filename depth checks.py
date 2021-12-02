c1757f9c5e00370e31b63eb1beb95ce173bbf322
= <=>
2021-12-02 10:09:58 -0500

    Initializing

---
+++ depth checks.py
@@ -0,0 +1,40 @@
+import pandas as pd
+from pandas.core.indexes.base import Index
+
+# Read file
+depth = pd.read_csv('depth.txt', sep=" ", header=None)
+depth = pd.DataFrame(depth)
+depth.columns = ['depths']
+# print(depth)
+increase = 0
+
+# function to add depth windows
+
+
+def findCurrentDepth(d, i):
+    # create globals to pass back to main
+    global depth1
+    global depth2
+    # initialize variables
+    depth1 = 0
+    depth2 = 0
+    # iterate through adding depths
+    for finder in range(3):
+        print(depth['depths'][i])
+        depth1 = depth1 + depth['depths'][i]
+        depth2 = depth2 + depth['depths'][d]
+        i = i+1
+        d = d+1
+    return(depth1, depth2)
+
+
+for i in range(len(depth)):
+    d = i+1
+    if d < len(depth)-2:
+        newDepths = findCurrentDepth(d, i)
+        if depth2 > depth1:
+            #print(d, i)
+            increase = increase + 1
+        # print(increase)
+
+print(increase)
